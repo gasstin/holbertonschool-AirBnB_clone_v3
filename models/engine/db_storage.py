756037e9ce457d3271a410953ba7f501e13e2acd
gasstin <gastonl1994@gmail.com>
2022-12-05 16:57:22 +0000

    adding task 3

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,24 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID,
+        or None if not found"""
+        for object_to_check in self.__session.query(classes[cls]).all():
+            if object_to_check.id == id:
+                return object_to_check
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching
+        the given class. If no class is passed,
+        returns the count of all objects in storage."""
+        if cls:
+            #classes_to_check = {cls: classes.get(cls)}
+            return len(self.__session.query(classes[cls]).all())
+        else:
+            len(self.__session.query(classes_).all()
+        # counter = 0
+        # for clase_auxiliar in classes_to_check:
+        #     counter += len(self.__session.query(classes_).all())
