1ed2fd47dcdaf8f6c5853fc53867367a09813f41
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-12-07 14:04:40 -0300

    adding endopint function to task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,23 @@
 """ to check the status """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """ return the status of your API """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def num_obj_bytype():
+    """endpoint that retrives the number"""
+    dic = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(dic)
