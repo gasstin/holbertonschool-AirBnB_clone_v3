e95c2504565f4103144a418029362fa9babb2582
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-12-07 14:40:19 -0300

    adding stets id collect and delte function task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """State objects that handles all default RESTFul API actions"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -13,3 +13,23 @@ def get_task():
     for state in storage.all('State').values():
         states.append(state.to_dict())
         return jsonify(states)
+
+@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
+def get__task_id(state_id):
+    """get states id"""
+    state_permision = storage.get('State', state_id)
+    if state_permision is None:
+        abort(404)
+    else:
+        return jsonify(state_permision.to_dict())
+
+@app_views.route("/states/<state_id>", methods=['DELETE'], strict_slashes=False)
+def get__task_delete(state_id):
+    """delete task"""
+    state_permision = storage.get('State', state_id)
+    if state_permision is None:
+        abort(404)
+    else:
+        storage.delete(state_permision)
+        storage.save()
+        return jsonify({}), 200
