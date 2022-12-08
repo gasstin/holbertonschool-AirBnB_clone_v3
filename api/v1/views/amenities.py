3f4950dd9c2b52f26c980ae7f8f6e0886cc2aa6c
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-12-08 01:35:32 -0300

    amenity first to get and get id

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,7 +5,7 @@ from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 
-@app_views.route('/amenities/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amentities(amenities_id):
     """Retrieves a list of all amenities"""
     all_amenities = storage.all(Amenity).values()
@@ -13,3 +13,12 @@ def get_amentities(amenities_id):
     for amenity in all_amenities:
         amenities_list.append(amenity.to_dict())
         return jsonify(amenities_list)
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def get_amenity(amenities_id):
+    """"" Retrieves an amenity """
+    amenities_get = storage.get(Amenity, amenities_id)
+    if not amenities_get:
+        abort(404)
+    else:
+        return jsonify(amenities_get.to_dict)
