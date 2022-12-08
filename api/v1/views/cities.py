62ef8e661e7220a4353f8eb0ba95b9fadbe3af56
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-12-08 00:59:38 -0300

    me quiero mimir pero me faltan task aaaaa

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -59,3 +59,21 @@ def post_city(state_id):
     instance.state_id = state.id
     instance.save()
     return make_response(jsonify(instance.to_dict()), 201)
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def put_city(city_id):
+    """
+    Updates a City
+    """
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    ignore = ['id', 'state_id', 'created_at', 'updated_at']
+    resq_json = request.get_json()
+    for key, value in resq_json.items():
+        if key not in ignore:
+            setattr(city, key, value)
+    storage.save()
+    return make_response(jsonify(city.to_dict()), 200)
