b9bca3b518efdb00818999047f39e06991cd763a
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-12-07 23:49:10 -0300

    adding make response to the functions

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,36 +6,39 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
-@app_views.route("/states", methods=['GET'], strict_slashes=False)
-def get__task():
-    """Show States"""
-    states = []
-    for state in storage.all('State').values():
-        states.append(state.to_dict())
-    return jsonify(states)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """
+    Retrieves the list of all State objects
+    """
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
 
 
-@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
-def get__task_id(state_id):
-    """Get id of a task"""
-    state_arr = storage.get('State', state_id)
-    if state_arr is None:
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ Retrieves a specific State """
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    else:
-        return jsonify(state_arr.to_dict())
+    return jsonify(state.to_dict())
 
 
-@app_views.route("/states/<state_id>", methods=['DELETE'],
-                 strict_slashes=False)
-def get__task_delete(state_id):
-    """Delete task"""
-    state_arr = storage.get('State', state_id)
-    if state_arr is None:
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """
+    Deletes a State Object
+    """
+    state = storage.get(State, state_id)
+
+    if not state:
         abort(404)
-    else:
-        storage.delete(state_arr)
-        storage.save()
-        return jsonify({}), 200
+    storage.delete(state)
+    storage.save()
+    return make_response(jsonify({}), 200)
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
